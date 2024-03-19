76809053e312311fab81521de747e57304c70279
Fernando Garcia <f.garcia@fourco.nl>
2024-03-19 10:39:03 +0100

    add increment helper function and the unit tests

--- hello-world-app/README.md
+++ hello-world-app/README.md
@@ -32,3 +32,23 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+### unit testing
+
+```typescript
+import { increment } from './helpers'
+
+describe('increment', () => {
+  test('increments the current number by 1', () => {
+    expect(increment(0, 10)).toBe(1)
+  })
+
+  test('does not increment the current number over the max', () => {
+    expect(increment(10, 10)).toBe(10)
+  })
+
+  test('has a default max of 10', () => {
+    expect(increment(10)).toBe(10)
+  })
+})
+```
--- hello-world-app/tests/unit/example.spec.ts
+++ hello-world-app/tests/unit/example.spec.ts
@@ -1,5 +1,6 @@
 import { shallowMount } from "@vue/test-utils";
 import HelloWorld from "@/components/HelloWorld.vue";
+import { increment } from "@/shared/helper";
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
@@ -8,5 +9,19 @@ describe("HelloWorld.vue", () => {
       props: { msg },
     });
     expect(wrapper.text()).toMatch(msg);
+  });
+});
+
+describe("increment", () => {
+  test("increments the current number by 1", () => {
+    expect(increment(0, 10)).toBe(1);
+  });
+
+  test("does not increment the current number over the max", () => {
+    expect(increment(10, 10)).toBe(10);
+  });
+
+  test("has a default max of 10", () => {
+    expect(increment(10)).toBe(10);
   });
 });
